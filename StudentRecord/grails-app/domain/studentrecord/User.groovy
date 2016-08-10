9900473c4cd0ed366112c68b57e17c8e68942bff
xaw-roz <saroj.shrestha@deerwalk.edu.np>
2016-08-10 13:07:08 +0545

    configured databasse

--- StudentRecord/grails-app/domain/studentrecord/User.groovy
+++ StudentRecord/grails-app/domain/studentrecord/User.groovy
@@ -1,7 +1,11 @@
 package studentrecord
 
 class User {
+    String email_id;
+    String password_hash;
 
     static constraints = {
+        email_id email: true,blank: false
+        password_hash blank: false
     }
 }
